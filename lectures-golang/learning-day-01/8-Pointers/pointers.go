c23dc7085e8c77eecae7f536b74b4e0de1d9c192
Aline <alinesresende14@gmail.com>
2022-12-19 18:48:48 -0300

    Learning about pointers in Golang.

--- lectures-golang/learning-day-01/8-Pointers/pointers.go
+++ lectures-golang/learning-day-01/8-Pointers/pointers.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// Pointer is a memory reference
+	// Use of the "&"" symbol means that the pointer variable is storing the variable address
+	// The value received is the address of the variable
+	var number int
+	var pointer *int
+	number = 100
+	pointer = &number
+	fmt.Println(number, *pointer)
+
+	// Another example
+
+	var creature string = "shark"
+	var pointer1 *string = &creature
+
+	fmt.Println("creature =", creature)
+	fmt.Println("pointer =", pointer1)
+
+	fmt.Println("*pointer =", *pointer1)
+
+	*pointer1 = "jellyfish"
+	fmt.Println("*pointer =", pointer1)
+	fmt.Println("creature =", creature)
 }
