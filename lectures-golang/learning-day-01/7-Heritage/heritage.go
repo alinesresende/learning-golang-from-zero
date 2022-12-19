45d69d1dc571522de1d117d903f5ad5ad12a1522
Aline <alinesresende14@gmail.com>
2022-12-19 17:57:46 -0300

    Understand about inheritance in structure.

--- lectures-golang/learning-day-01/7-Heritage/heritage.go
+++ lectures-golang/learning-day-01/7-Heritage/heritage.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+type people struct {
+	name     string
+	lastName string
+	age      uint8
+	height   uint8
+}
+
+// writes struct name without passing types
+type student struct {
+	people  //
+	course  string
+	college string
+}
+
+func main() {
+	p1 := people{"João", "Pedro", 20, 178}
+	fmt.Println(p1)
+	s1 := student{p1, "Engenharia", "USP"}
+	fmt.Println(s1)
+
+	// access form or struct
+	fmt.Println(s1.name)
+	fmt.Println(s1.height)
 }
