505dd16e5de199a57d3104d6173f7f400e0dfd4c
Aline <alinesresende14@gmail.com>
2022-12-19 16:00:57 -0300

    Concludes the study of several operators in Golang.

--- lectures-golang/learning-day-01/5-Operators/operators.go
+++ lectures-golang/learning-day-01/5-Operators/operators.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// Arithmetic operators
@@ -11,4 +13,39 @@ func main() {
 	restOfDivision := 10 % 2
 
 	fmt.Println(sum, subtraction, division, multiplication, restOfDivision)
+	// It is not possible to perform calculations with different types
+	// example: sum type int8 + int32
+	var number1 int16 = 10
+	var number2 int16 = 25
+	sum2 := number1 + number2
+	fmt.Println(sum2)
+
+	// Assignment Operators
+	var text string = "String"
+	text2 := "String2"
+	fmt.Println(text, text2)
+
+	// Relational Operators
+	// return a boolean (true or false)
+	fmt.Println(1 > 2)  // false
+	fmt.Println(1 >= 2) // false
+	fmt.Println(1 == 2) // false
+	fmt.Println(1 <= 2) // true
+	fmt.Println(1 < 2)  // true
+	fmt.Println(1 != 2) // true
+
+	// Logical Operators
+	truth, lie := true, false
+	fmt.Println(truth && lie) // false
+	fmt.Println(truth || lie) // true
+	fmt.Println(!truth)       // false
+	fmt.Println(!lie)         // true
+
+	// Unary operators
+	numberOne := 10
+	numberOne++
+	numberOne += 15
+	fmt.Println(numberOne)
+
+	// there is no ternary operator in Golang
 }
