7028482f9d07cb2e2519deedc8858f354c2b0b24
Aline <alinesresende14@gmail.com>
2022-12-19 17:29:29 -0300

    Learning about struct and functioning.

---
+++ lectures-golang/learning-day-01/6-Structs/structs.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+// syntax using type declaration, followed by name and struct
+type user struct {
+	name    string
+	age     uint8
+	address address
+}
+
+type address struct {
+	publicPlace string
+	number      uint8
+}
+
+func main() {
+
+	var u user
+	u.name = "Aline"
+	u.age = 21
+	fmt.Println(u)
+
+	// Another way to use struct
+	addressExample := address{"Street", 25}
+	userTwo := user{"Aline", 21, addressExample}
+	fmt.Println(userTwo)
+
+	// Another way to use struct without all the data
+	// The field without information has a blank result
+	userThree := user{name: "Aline"}
+	fmt.Println(userThree)
+
+	// Is it possible to have struct inside struct
+}
