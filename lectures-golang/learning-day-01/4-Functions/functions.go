0efd9adfd63e272f76a9cddc4b6489cb8a981acc
Aline <alinesresende14@gmail.com>
2022-12-19 14:33:53 -0300

    Different forms of function declaration.

--- lectures-golang/learning-day-01/4-Functions/functions.go
+++ lectures-golang/learning-day-01/4-Functions/functions.go
@@ -7,7 +7,34 @@ func somar(n1 int8, n2 int8) int8 {
 	return n1 + n2
 }
 
+// Functions with more than one return
+func calculations(n1, n2 int8) (int8, int8) {
+	sum := n1 + n2
+	subtraction := n1 - n2
+	return sum, subtraction
+}
+
 func main() {
 	soma := somar(10, 20)
 	fmt.Println(soma)
+
+	// Declare variable of type function and cast a function inside
+	var exampleFunc = func() {
+		fmt.Println("Function inside variable")
+	}
+	exampleFunc()
+
+	// Another example
+
+	var newExamplo = func(text string) {
+		fmt.Println(text)
+	}
+	newExamplo("New Function")
+
+	// when I don't want to use a variable I use "_"
+	_, resultSubtraction := calculations(10, 15)
+	fmt.Println(resultSubtraction)
+
+	resultSum, resultSubtraction := calculations(10, 15)
+	fmt.Println(resultSum, resultSubtraction)
 }
