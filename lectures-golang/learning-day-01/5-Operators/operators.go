bfc2f38dda9bf6df9b7b3dcc81679aa542cfb564
Aline <alinesresende14@gmail.com>
2022-12-19 14:47:50 -0300

    Renaming folders.

---
+++ lectures-golang/learning-day-01/5-Operators/operators.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Arithmetic operators
+	sum := 1 + 2
+	subtraction := 1 - 2
+	division := 10 / 4
+	multiplication := 10 * 5
+	restOfDivision := 10 % 2
+
+	fmt.Println(sum, subtraction, division, multiplication, restOfDivision)
+}
