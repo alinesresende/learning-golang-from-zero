a1fae51fce87ec3f391121e6d9f779f9574b9e13
Aline <alinesresende14@gmail.com>
2022-12-20 19:38:48 -0300

    Noting the Differences Between Array and Slice.

--- lectures-golang/learning-go/9-Arrays-Slices/internal-arrays.go
+++ lectures-golang/learning-go/9-Arrays-Slices/internal-arrays.go
@@ -1,6 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	// Internal Arrays
+	// Syntax
+	// function make: type + length + capacity
+	// slice_name := make([]type, length, capacity)
+	// The make function creates a slice with the informed capacity and returns the length
+	// If the capacity parameter is not defined, it will be equal to length.
+	slice := make([]float32, 10, 11)
+	fmt.Println(slice)
+	fmt.Println(len(slice)) // length
+	fmt.Println(cap(slice)) // capacity
 
+	slice = append(slice, 5)
+	fmt.Println(slice)
 }
