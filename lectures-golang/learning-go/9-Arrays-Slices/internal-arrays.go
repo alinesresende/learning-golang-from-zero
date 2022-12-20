c6c46a0a5d29d9926ba7f3cf3945884f2af07dd9
Aline <alinesresende14@gmail.com>
2022-12-20 20:48:25 -0300

    Learning about Slice.

--- lectures-golang/learning-go/9-Arrays-Slices/internal-arrays.go
+++ lectures-golang/learning-go/9-Arrays-Slices/internal-arrays.go
@@ -15,5 +15,23 @@ func main() {
 	fmt.Println(cap(slice)) // capacity
 
 	slice = append(slice, 5)
+	slice = append(slice, 6)
+	fmt.Println(len(slice)) // length
+	fmt.Println(cap(slice)) // capacity
 	fmt.Println(slice)
+	fmt.Println("--------------")
+
+	slice1 := make([]float32, 3)
+	fmt.Println(len(slice1)) // length
+	fmt.Println(cap(slice1)) // capacity
+	fmt.Println("--------------")
+
+	slice1 = append(slice1, 4)
+	slice1 = append(slice1, 5)
+	slice1 = append(slice1, 6)
+	slice1 = append(slice1, 7)
+	fmt.Println(len(slice1)) // length
+	fmt.Println(cap(slice1)) // capacity
+	fmt.Println(slice1)
+
 }
