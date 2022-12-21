65a66847cc0aad3d942557478628e767bed0f3d2
Aline <alinesresende14@gmail.com>
2022-12-21 17:59:07 -0300

    Content about named functions in Golang.

--- lectures-golang/learning-second/14-Advanced-Functions/named-return.go
+++ lectures-golang/learning-second/14-Advanced-Functions/named-return.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+// Functions with named return
+// Return values ​​in Golang can be named and act like variables.
+// The return statement with no arguments returns the current values ​​of the results. This is known as a "clean" return.
+
+func mathematicalCalculations(n1, n2 int) (sum int, subtraction int) {
+	sum = n1 + n2
+	subtraction = n1 - n2
+	return // It is not necessary to write the name of the variables
+}
+
+func main() {
+	sum, subtraction := mathematicalCalculations(10, 20)
+	fmt.Println(sum, subtraction)
 }
