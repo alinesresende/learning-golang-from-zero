d6480f1ed12fc021e7d5e6b56c08a5ff530a0d87
Aline <alinesresende14@gmail.com>
2022-12-21 17:30:15 -0300

    Loop content in Golang finished.

--- lectures-golang/learning-second/13-Loops/loops.go
+++ lectures-golang/learning-second/13-Loops/loops.go
@@ -2,22 +2,52 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	// Repeating structure
 	// First example of FOR
+
 	number := 0
 	for number < 10 {
 		number++
 		fmt.Println("Incrementing number")
-		time.Sleep(time.Second)
 	}
 
-	// Secund example of FOR
+	// // Secund example of FOR
+	// // The increment can be with other numbers
+
 	for numberTwo := 0; numberTwo < 10; numberTwo++ {
 		fmt.Println("Incrementing numberTwo", numberTwo)
-		time.Sleep(time.Second)
 	}
+
+	// FOR with range clause
+	// The range clause is used to iterate over something
+	// Example: map, string...
+	// The index and value declaration is mandatory
+	// If you don't want to use the index or value, you need to put a "_" in its place
+
+	names := [3]string{"Aline", "Thais", "Vera"}
+	for index, name := range names {
+		fmt.Println(index, name)
+	}
+
+	// Interaction by String
+	// it is necessary to put String to be able to visualize the content
+
+	for index, letter := range "WORD" {
+		fmt.Println(index, string(letter))
+	}
+
+	// Interaction by Map
+
+	user := map[string]string{
+		"name":      "Aline",
+		"lastename": "Resende",
+	}
+	for key, value := range user {
+		fmt.Println(key, value)
+	}
+
+	// note: Unable to range in Struct
 }
