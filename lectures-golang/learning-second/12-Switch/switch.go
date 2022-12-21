7b0a342538d117a430d72b3afcef8e9b13468f35
Aline <alinesresende14@gmail.com>
2022-12-21 15:29:50 -0300

    Switch Case and Fallthrough Concepts.

---
+++ lectures-golang/learning-second/12-Switch/switch.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+)
+
+func dayOfTheWeek(number int) string {
+	switch number {
+	case 1:
+		return "Sunday"
+	case 2:
+		return "Monday"
+	case 3:
+		return "Tuesday"
+	case 4:
+		return "Wednesday"
+	case 5:
+		return "Thursday"
+	case 6:
+		return "Friday"
+	case 7:
+		return "Saturday"
+	default:
+		return "Invalid number"
+	}
+}
+
+// Another way to assess the condition of the Switch
+// fallthrough: Transfers control to the next case, even if the current case may have matched.
+func dayOfTheWeekTwo(number int) string {
+	switch {
+	case number == 1:
+		return "Sunday"
+	case number == 2:
+		return "Monday"
+	case number == 3:
+		return "Tuesday"
+	case number == 4:
+		return "Wednesday"
+	case number == 5:
+		return "Thursday"
+	case number == 6:
+		return "Friday"
+	case number == 7:
+		return "Saturday"
+	default:
+		return "Invalid number"
+	}
+}
+
+func main() {
+	day := dayOfTheWeek(200)
+	fmt.Println(day)
+
+	dayTwo := dayOfTheWeekTwo(5)
+	fmt.Println(dayTwo)
+}
