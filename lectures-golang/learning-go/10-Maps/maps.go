917d9ed0cefd3dc55facdf134c92a86157dbaa40
Aline <alinesresende14@gmail.com>
2022-12-20 21:39:22 -0300

    Finished map content.

--- lectures-golang/learning-go/10-Maps/maps.go
+++ lectures-golang/learning-go/10-Maps/maps.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// inside the "[]" type of the curly braces
+	// outside the "[]" type of the values
+	// note: both fields need to be the same type
+	// Example: map[string]string
+	// it's forbidden: map[string]int
+	user := map[string]string{
+		"name":     "Pedro",
+		"lastname": "Silva",
+	}
+	fmt.Println(user)
+
+	// To access
+	fmt.Println(user["name"])
+
+	// Nested map
+	user2 := map[string]map[string]string{
+		"name": {
+			"first": "Aline",
+			"last":  "Resende",
+		},
+		"course": {
+			"name":   "Engenharia",
+			"fildes": "fildes1",
+		},
+	}
+	fmt.Println(user2)
+
+	// To delete
+	delete(user2, "name")
+	fmt.Println(user2)
 }
