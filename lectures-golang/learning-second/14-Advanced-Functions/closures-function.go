75e3e97799e7753533a5696004c2ce355877dcb8
Aline <alinesresende14@gmail.com>
2022-12-23 19:53:33 -0300

    Study on Closure Function in Golang.

--- lectures-golang/learning-second/14-Advanced-Functions/closures-function.go
+++ lectures-golang/learning-second/14-Advanced-Functions/closures-function.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
+// A closure function is a function that takes another function.
+// The function "exampleFunction" is using a variable "text" that is outside the function.
+func closure() func() {
+	text := "Inside the closures function"
+
+	exampleFunction := func() {
+		fmt.Println(text)
+	}
+	return exampleFunction
+}
+
 func main() {
 	// Closures Function
+	// These are functions that reference variables that are outside their scope.
+	text := "Inside the main function"
+	fmt.Println(text)
+
+	functionNew := closure()
+	functionNew()
 }
