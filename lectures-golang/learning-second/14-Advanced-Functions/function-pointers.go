c23eaa61dc0177f492d5150b44d747e4382f211c
Aline <alinesresende14@gmail.com>
2022-12-26 12:19:53 -0300

    function pointers content.

--- lectures-golang/learning-second/14-Advanced-Functions/function-pointers.go
+++ lectures-golang/learning-second/14-Advanced-Functions/function-pointers.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+func invertSign(number int) int {
+	return number * -1
+}
+
+func invertSignWithPointers(number *int) {
+	*number = *number * -1
+}
+
 func main() {
 	// Function with pointers
+	number := 20
+	invertNumber := invertSign(number)
+	fmt.Println(invertNumber)
+
+	fmt.Println("-----------")
+
+	newNumber := 40
+	fmt.Println(newNumber)
+	invertSignWithPointers((&newNumber))
+	fmt.Println(newNumber)
 }
