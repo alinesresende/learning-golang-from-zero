c35a5adae13a1f87794025d73e30faf4eebf45a8
Aline <alinesresende14@gmail.com>
2023-01-02 14:31:20 -0300

    Organizing the name of Golang language study files.

--- lectures-golang/competition/go.mod
+++ lectures-golang/competition/go.mod
@@ -1,3 +1,3 @@
-module module
+module moduleTwo
 
 go 1.19
--- lectures-golang/competition/goroutines.go
+++ lectures-golang/competition/goroutines.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	// competition != parallelism
+	// In parallelism, tasks are executed at the same time.
+	// In concurrency, tasks may or may not be executed at the same time.
+
+	write("Hi, world!")
+}
+
+func write(text string) {
+	for {
+		fmt.Println(text)
+		time.Sleep(time.Second)
+	}
 }
