c32cc9e5a43ba5681b31c0ad479826835ddcea14
Aline <alinesresende14@gmail.com>
2023-01-02 15:52:51 -0300

    Starting goroutines content in Golang.

--- lectures-golang/concurrency/goroutines.go
+++ lectures-golang/concurrency/goroutines.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("hi")
+	// concurrency != parallelism
+	// Concurrency is the ability to handle several things at once
+	// Parallelism is the ability to handle multiple things at the same time
+	// Goroutines are functions or methods that run concurrently
+	// To use Goroutines it is necessary to include "go" before the function.
+
+	go write("Hi, world!") // using goroutines
+	write("Programming in golang")
+}
+
+func write(text string) {
+	for {
+		fmt.Println(text)
+		time.Sleep(time.Second)
+	}
 }
