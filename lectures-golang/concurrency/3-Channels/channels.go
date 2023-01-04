76a303cfaf2861c77b397553bdd79fe238f8cffe
Aline <alinesresende14@gmail.com>
2023-01-04 15:49:29 -0300

    Study of the content of channels in Golang.

--- lectures-golang/concurrency/3-Channels/channels.go
+++ lectures-golang/concurrency/3-Channels/channels.go
@@ -5,17 +5,28 @@ import (
 	"time"
 )
 
+// Channel
+// The channel can send and receive data to organize the goroutines
+// To create a channel it is necessary to use the make() function, with the word "chan" and then specify a type (ex: string)
+// The use of "<-" serves to send something
+
 func main() {
 	channel := make(chan string)
 	go write("hi, world!", channel)
 
-	message := <-channel
-	fmt.Println(message)
+	fmt.Println("After the function starts executing")
+
+	for message := range channel {
+		fmt.Println(message)
+	}
+
+	fmt.Println("End of program")
 }
 
 func write(text string, channel chan string) {
-	for i := 0; 1 < 5; i++ {
-		channel <- text
+	for i := 0; i < 5; i++ {
+		channel <- text // sending to channel
 		time.Sleep(time.Second)
 	}
+	close(channel)
 }
